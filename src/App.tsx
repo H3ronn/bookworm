242b7c00e82035a0ab2f35cf378418d899fc1f91
Bartosz Czaplicki <h3ronnn@gmail.com>
2022-06-14 11:45:22 +0200

    feat: add basic fetch books and display list of books

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return <h1>BookWorm</h1>;
+interface IResources {
+  id: number;
+  uri: string;
+  type: string;
 }
+
+interface IBook {
+  id: number;
+  description: string;
+  title: string;
+  subjects: string[];
+  resources: IResources[];
+}
+
+const App = () => {
+  const [data, setData] = useState<IBook[] | null>(null);
+  const [page, setPage] = useState(1);
+
+  const nextPage = (): void => {
+    setPage((prev) => prev + 1);
+  };
+
+  const prevPage = (): void => {
+    setPage((prev) => prev - 1);
+  };
+
+  useEffect(() => {
+    axios.get(`https://gnikdroy.pythonanywhere.com/api/book?page=${page}`).then(({ data }) => {
+      setData(data.results);
+      console.log('strzal do api');
+    });
+  }, [page, setData]);
+  return (
+    <div>
+      <h1>BookWorm</h1>
+      <button onClick={prevPage}>Prev page</button>
+      <button onClick={nextPage}>Next page</button>
+      {!!data &&
+        data.map((book, i) => {
+          const imgLink = book.resources.find(
+            (resource) => resource.type === 'image/jpeg' && resource.uri.includes('.medium.'),
+          )?.uri;
+          return (
+            <div key={book.id}>
+              <h4>{i + 1 + '. ' + book.title}</h4>
+              <img src={imgLink} alt="" />
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 
 export default App;
