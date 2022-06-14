3df5b66ff4afb44f779676725081b479afad5579
Bartosz Czaplicki <h3ronnn@gmail.com>
2022-06-14 12:14:12 +0200

    feat: implement function to mark book as favourite

--- src/App.tsx
+++ src/App.tsx
@@ -18,21 +18,41 @@ interface IBook {
 const App = () => {
   const [data, setData] = useState<IBook[] | null>(null);
   const [page, setPage] = useState(1);
+  const [favorite, setFavorite] = useState<number[]>([]);
 
-  const nextPage = (): void => {
+  const nextPage = () => {
     setPage((prev) => prev + 1);
   };
 
-  const prevPage = (): void => {
+  const prevPage = () => {
     setPage((prev) => prev - 1);
+  };
+
+  const toggleFavorite = (bookId: number) => {
+    if (favorite.includes(bookId)) {
+      const filteredFavourite = favorite.filter((favId) => favId !== bookId);
+      setFavorite(filteredFavourite);
+    } else {
+      setFavorite((prev) => [...prev, bookId]);
+    }
+  };
+
+  const isFavorite = (id: number): boolean => {
+    return favorite.includes(id);
   };
 
   useEffect(() => {
     axios.get(`https://gnikdroy.pythonanywhere.com/api/book?page=${page}`).then(({ data }) => {
       setData(data.results);
+      console.log(data.results[0]);
       console.log('strzal do api');
     });
   }, [page, setData]);
+
+  useEffect(() => {
+    console.log(favorite);
+  }, [favorite]);
+
   return (
     <div>
       <h1>BookWorm</h1>
@@ -46,7 +66,11 @@ const App = () => {
           return (
             <div key={book.id}>
               <h4>{i + 1 + '. ' + book.title}</h4>
-              <img src={imgLink} alt="" />
+              <button onClick={() => toggleFavorite(book.id)}>
+                {isFavorite(book.id) ? 'Delete from favorite' : 'Make as favourite'}
+              </button>
+              <img style={{ display: 'block' }} src={imgLink} alt="" />
+              <p>{book.description}</p>
             </div>
           );
         })}
